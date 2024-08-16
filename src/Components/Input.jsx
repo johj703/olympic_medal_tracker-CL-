3868bd23b676c24e1162c4acca31e976dbb781a1
Jo <johj703@nate.com>
2024-08-16 12:36:41 +0900

    국가명, 금은동 메달 input 속성 추가

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -3,19 +3,43 @@ const Input = (addCountries) => {
     <>
       <div>
         <p>국가명</p>
-        <input />
+        <input
+          type="text"
+          name="country"
+          placeholder="국가 입력"
+          value={FormData.countries}
+          required
+        />
       </div>
       <div>
         <p>금메달</p>
-        <input />
+        <input
+          type="number"
+          name="gold"
+          placeholder="금메달 수"
+          value={FormData.gold}
+          required
+        />
       </div>
       <div>
         <p>은메달</p>
-        <input />
+        <input
+          type="number"
+          name="silver"
+          placeholder="은메달 수"
+          value={FormData.silver}
+          required
+        />
       </div>
       <div>
         <p>동메달</p>
-        <input />
+        <input
+          type="number"
+          name="bronze"
+          placeholder="동메달 수"
+          value={FormData.bronze}
+          required
+        />
       </div>
       <div>
         <button onClick={addCountries}>국가 추가</button>
