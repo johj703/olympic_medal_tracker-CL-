cb2f21be0f52bf66a9cf9901b6e1cd2f6bb70f34
Jo <johj703@nate.com>
2024-08-16 11:56:34 +0900

    Table 컴포넌트에 내려줄 country를 props로 table 컴포넌트에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import useState from "react";
 import Header from "./Components/Header";
 import Input from "./Components/Input";
 import Table from "./Components/Table";
 
 function App() {
+  const [country, setCountry] = useState([]);
   return (
     <>
       <Header />
       <Input />
-      <Table />
+      <Table country={country} />
     </>
   );
 }
