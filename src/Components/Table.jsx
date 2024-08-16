e683ba144df7d7a066c34a22c80190495b984009
Jo <johj703@nate.com>
2024-08-16 12:06:46 +0900

    countries를 map을 통해 테이블에 추가할 수 있도록 작성

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -16,13 +16,15 @@ const Table = ({ countries }) => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>국가명</th>
-              <th>금메달</th>
-              <th>은메달</th>
-              <th>동메달</th>
-              <th>액션</th>
-            </tr>
+            {countries.map((counties, index) => (
+              <tr key={index}>
+                <th>국가명</th>
+                <th>금메달</th>
+                <th>은메달</th>
+                <th>동메달</th>
+                <th>액션</th>
+              </tr>
+            ))}
           </tbody>
         </table>
       )}
