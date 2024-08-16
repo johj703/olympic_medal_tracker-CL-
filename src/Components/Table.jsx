6f932496e173ddd702b4339750ef9efa1697f441
Jo <johj703@nate.com>
2024-08-16 11:58:44 +0900

    countries의 길이가 0일 때라는 조건으로 변경

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -2,7 +2,7 @@
 const Table = ({ countries }) => {
   return (
     <>
-      {countries ? (
+      {countries.length === 0 ? (
         <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
       ) : (
         <div>
